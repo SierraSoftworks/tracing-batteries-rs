c3b8188f656bccafe204cb8f5e09fb42052ac777
Benjamin Pannell <benjamin@pannell.dev>
2024-09-08 01:56:06 +0100

    feat: Export Span and Event for tracing

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,5 +1,5 @@
 pub use tracing::{
     debug, debug_span, error, error_span, event,
     field::{debug, display},
-    info, info_span, span, trace, trace_span, warn, warn_span, Instrument,
+    info, info_span, span, trace, trace_span, warn, warn_span, Event, Instrument, Span,
 };
