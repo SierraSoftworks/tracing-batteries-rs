d66326af1b3ba1fcfbec92e7bdb4f77c7ac4684c
Benjamin Pannell <benjamin@pannell.dev>
2024-09-08 02:10:53 +0100

    tweak: Further changes to re-exported types

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,6 @@
 pub use tracing::{
     debug, debug_span, error, error_span, event,
-    field::{debug, display},
+    field::{debug, display, Empty as EmptyField},
     info, info_span, span, trace, trace_span, warn, warn_span, Event, Instrument, Span,
 };
 
