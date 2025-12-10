b3d1722162bea5b82a165d841d6bdbf7fb451507
Benjamin Pannell <benjamin@pannell.dev>
2025-12-10 23:52:37 +0000

    feat: Expose OpenTelemetry propagation interfaces in prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,14 +1,23 @@
 pub use tracing::{
-    self, debug, debug_span, error, error_span, event,
-    field::{debug, display, Empty as EmptyField},
-    info, info_span, span, trace, trace_span, warn, warn_span, Event, instrument, Instrument, Span,
+    self, Event, Instrument, Span, debug, debug_span, error, error_span, event,
+    field::{Empty as EmptyField, debug, display},
+    info, info_span, instrument, span, trace, trace_span, warn, warn_span,
 };
 
 #[cfg(feature = "opentelemetry")]
 pub use tracing_opentelemetry::{self, OpenTelemetrySpanExt};
 
 #[cfg(feature = "opentelemetry")]
-pub use opentelemetry::{self, trace::SpanKind as OpenTelemetrySpanKind, trace::TraceContextExt};
+pub use opentelemetry::{
+    self,
+    global::{get_text_map_propagator, set_text_map_propagator},
+    propagation::{
+        Extractor as OpenTelemetryPropagationExtractor,
+        Injector as OpenTelemetryPropagationInjector,
+    },
+    trace::SpanKind as OpenTelemetrySpanKind,
+    trace::TraceContextExt,
+};
 
 #[cfg(feature = "opentelemetry")]
 pub use opentelemetry_sdk::propagation::TraceContextPropagator;
